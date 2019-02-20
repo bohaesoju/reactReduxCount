3dd5e3770fd27a0b0cb5f3e1d5db9e4f35570a81
kimuyjoong <fr_bohaesoju@woowahan.com>
2019-02-20 13:40:10 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react';
 
 import './App.css';
 import Counter from './components/Counter';
-import Palette from './components/Palette';
-import WaitingList from './components/WaitingList';
+import PaletteContainer from './containers/PaletteContainer';
+import WaitingListContainer from './containers/WaitingListContainer';
+import CounterContainer from "./containers/CounterContainer";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Palette selected="red" />
-        <Counter value={0} color="red" />
-        <WaitingList />
+        <PaletteContainer />
+        <CounterContainer />
+        <WaitingListContainer />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// **** (1) createStore 와 루트 리듀서 불러오기
+import { createStore } from 'redux';
+import rootReducer from './store/modules';
+// **** (1) Provider 불러오기
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// **** 리덕스 개발자도구 적용
+const devTools =
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, devTools);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
 registerServiceWorker();
